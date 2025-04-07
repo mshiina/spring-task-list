32ca8ebc67b19946e95c7daf78f9d9272bdd1538
417MAIKA_PC\k017a <may09tosi@gmail.com>
2025-04-08 00:14:39 +0900

    eqlipsからコミット/プッシュ

---
+++ spring-task-list/src/main/resources/schema.sql
@@ -0,0 +1,23 @@
+-- 各種テーブル削除
+DROP TABLE IF EXISTS categories;
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS tasks;
+
+-- users テーブルを作成するクエリ
+CREATE TABLE users (
+id INTEGER PRIMARY KEY,
+email VARCHAR(255),
+name VARCHAR(20),
+password VARCHAR(50)
+);
+
+-- tasks テーブルを作成するクエリ
+CREATE TABLE tasks (
+id INTEGER PRIMARY KEY,
+category_id INTEGER,
+user_id INTEGER,
+title VARCHAR(255),
+closing_date DATE,
+progress INTEGER,
+memo TEXT
+);
