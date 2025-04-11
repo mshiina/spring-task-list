87ac200586bb5c4624561dcbd1a1b4e5afd9737c
417MAIKA_PC\k017a <may09tosi@gmail.com>
2025-04-12 00:19:34 +0900

    Task.javaにtasksテーブルと連携するEntityクラスを作成

--- spring-task-list/src/main/java/com/example/demo/entity/Task.java
+++ spring-task-list/src/main/java/com/example/demo/entity/Task.java
@@ -1,5 +1,50 @@
 package com.example.demo.entity;
 
-public class Task {
+import java.sql.Date;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tasks") //対応するテーブル名
+public class Task {
+	//フィールド
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY) //自動採番
+	private Integer id; //タスクID
+	@Column(name = "users_id") //テーブルのカラム名が異なる場合入れる
+	private Integer userId; //usersテーブルのID
+	private String title; //タイトル
+	private Date closing_date; //期限
+	private Integer progress; //進捗状況
+	private String memo; //メモ
+
+	//ゲッター
+	public Integer getId() {
+		return id;
+	}
+
+	public Integer getUserId() {
+		return userId;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public Date getClosingDate() {
+		return closing_date;
+	}
+
+	public Integer getProgress() {
+		return progress;
+	}
+
+	public String getMdmo() {
+		return memo;
+	}
 }
