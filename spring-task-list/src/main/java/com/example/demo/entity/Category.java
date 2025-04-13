dc954e97c9b287a544adc57f72250d0efd0429a4
417MAIKA_PC\k017a <may09tosi@gmail.com>
2025-04-13 17:20:02 +0900

    カテゴリーテーブルと連携

--- spring-task-list/src/main/java/com/example/demo/entity/Category.java
+++ spring-task-list/src/main/java/com/example/demo/entity/Category.java
@@ -1,5 +1,26 @@
 package com.example.demo.entity;
 
-public class Category {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name = "categories")
+public class Category {
+	//フィールド
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id; //カテゴリーID
+	private String name; //カテゴリー名
+
+	//ゲッター
+	public Integer getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
 }
