4acae7fab4d84c59b58b67525524236c9d24338b
417MAIKA_PC\k017a <may09tosi@gmail.com>
2025-04-13 12:05:39 +0900

    tasks.htmlにタスク一覧を表示するメソッドを設定

--- spring-task-list/src/main/java/com/example/demo/controller/TaskController.java
+++ spring-task-list/src/main/java/com/example/demo/controller/TaskController.java
@@ -1,5 +1,28 @@
 package com.example.demo.controller;
 
-public class TaskController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.example.demo.entity.Task;
+import com.example.demo.repository.TaskRepository;
+
+@Controller
+public class TaskController {
+	@Autowired
+	TaskRepository taskRepository; //tasksテーブル操作用
+
+	//タスク一覧を表示する
+	@GetMapping("/tasks")
+	public String index(Model model) {
+		//tasksテーブルから全タスクの一覧を取得
+		List<Task> taskList = taskRepository.findAll();
+		//Thymeleafにデータを渡す
+		model.addAttribute("tasks", taskList);
+		//tasks.htmlを出力
+		return "tasks";
+	}
 }
