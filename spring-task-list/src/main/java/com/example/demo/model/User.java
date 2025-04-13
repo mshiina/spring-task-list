aad5852aebac11e87f144d6c1cacd1589a3ccc08
417MAIKA_PC\k017a <may09tosi@gmail.com>
2025-04-13 14:12:15 +0900

    USer.javaにusersテーブルと連携するEntityクラスを作成

--- spring-task-list/src/main/java/com/example/demo/model/User.java
+++ spring-task-list/src/main/java/com/example/demo/model/User.java
@@ -1,5 +1,36 @@
 package com.example.demo.model;
 
-public class User {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name = "users") //対応するテーブル名
+public class User {
+	//フィールド
+	@Id //主キー
+	@GeneratedValue(strategy = GenerationType.IDENTITY) //自動採番
+	private Integer id;
+	private String email;
+	private String name;
+	private String password;
+
+	//ゲッター
+	public Integer getId() {
+		return id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getPassword() {
+		return password;
+	}
 }
