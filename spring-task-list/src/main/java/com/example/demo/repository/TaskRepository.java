04ad4ae1c9a62d58516af5a2685f29a9a922b375
417MAIKA_PC\k017a <may09tosi@gmail.com>
2025-04-13 17:37:12 +0900

    カテゴリー検索を設定

--- spring-task-list/src/main/java/com/example/demo/repository/TaskRepository.java
+++ spring-task-list/src/main/java/com/example/demo/repository/TaskRepository.java
@@ -1,9 +1,14 @@
 package com.example.demo.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.demo.entity.Task;
 
 public interface TaskRepository extends JpaRepository<Task, Integer> {
+	//カテゴリーIDによる検索
+	//SELECT * FROM tasks WHERE catetory_id = ?
+	List<Task> findByCategoryId(Integer categoryId);
 
 }
