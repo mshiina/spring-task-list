ecb30e8c2ce7951ec71db8744c66eee77457dcec
417MAIKA_PC\k017a <may09tosi@gmail.com>
2025-04-17 00:01:06 +0900

    新規登録処理と表示を作成（ラジオボタンデータの受け渡しはエラー発生中）

--- spring-task-list/src/main/java/com/example/demo/controller/TaskController.java
+++ spring-task-list/src/main/java/com/example/demo/controller/TaskController.java
@@ -1,11 +1,13 @@
 package com.example.demo.controller;
 
+import java.sql.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.demo.entity.Category;
@@ -43,4 +45,26 @@ public class TaskController {
 		return "tasks";
 	}
 
+	//新規登録画面の表示
+	@GetMapping("/tasks/add")
+	public String create() {
+		return "addTask";
+	}
+
+	//新規登録処理
+	@PostMapping("/tasks/add")
+	public String work(
+			@RequestParam(value = "categoryId", defaultValue = "") Integer categoryId,
+			@RequestParam(value = "title", defaultValue = "") String title,
+			@RequestParam(value = "closingDate", defaultValue = "") Date closingDate,
+			@RequestParam(value = "progress", defaultValue = "") Integer progress,
+			@RequestParam(value = "memo", defaultValue = "") String memo,
+			Model model) {
+		//Taskオブジェクトの作成
+		Task task = new Task(categoryId, title, closingDate, progress, memo);
+		//tasksテーブルへの反映(INSERT)
+		taskRepository.save(task);
+		//「/tasks」にGETでリクエストし直す（リダイレクト）
+		return "redirect:/tasks";
+	}
 }
