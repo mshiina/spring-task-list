369d959578754e3e7e49ee2dfc6c9e65423d6709
417MAIKA_PC\k017a <may09tosi@gmail.com>
2025-04-20 17:42:18 +0900

    自動生成のIDまで生成に含まれていたため、自動生成文を削除

--- spring-task-list/src/main/resources/data.sql
+++ spring-task-list/src/main/resources/data.sql
@@ -5,9 +5,9 @@ VALUES
 (2, 'suzuki@aaa.com', '鈴木一郎', 'test456');
 
 -- tasks テーブルにデータを挿入するクエリ
-INSERT INTO tasks (id, category_id, user_id, title, closing_date, progress, memo)
+INSERT INTO tasks (category_id, user_id, title, closing_date, progress, memo)
 VALUES
-(1, 1, 1, '見積もり', '2025-12-31', 0, '案件に適した見積もりを取る');
+(1, 1, '見積もり', '2025-12-31', 0, '案件に適した見積もりを取る');
 
 -- categories テーブルにデータを挿入するクエリ
 INSERT INTO categories(name) VALUES('仕事');
